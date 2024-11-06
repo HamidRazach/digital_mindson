1b382276bdf2b54efc8e06755b4e8820755b93c4
hamidRazach <150281909+HamidRazach@users.noreply.github.com>
2024-11-06 21:31:48 +0500

    back to top f arrow add

--- assets/js/main.js
+++ assets/js/main.js
@@ -693,6 +693,16 @@ lightToDarkButton?.addEventListener("click", function () {
     }
 });
 
+// /JavaScript to show/hide the Back to Top button
+        const backToTopButton = document.getElementById("backToTop");
 
+        window.addEventListener("scroll", function() {
+            if (window.scrollY > 200) { // Show after scrolling down 200px
+                backToTopButton.style.display = "flex";
+            } else {
+                backToTopButton.style.display = "none";
+            }
+        });
+ 
 
 
